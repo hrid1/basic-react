9fa66bb90d7600e180f1072f8657e51f72056510
hrid1 <hridoycse1@gmail.com>
2024-02-29 23:56:28 +0600

    this is rsc

---
+++ my-react-app/components/Countries/Countries.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+
+            <h1>Here are the countries.</h1>
+            
+        </div>
+    );
+};
+
+export default Countries;
---
+++ my-react-app/components/Person/Person.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Person = () => {
+    return (
+        <div>
+            <h2>This is  Person.</h2>
+        </div>
+    );
+};
+
+export default Person;
