3008bda65044b694ac3b8b21a32c0a96297b252e
hrid1 <hridoycse1@gmail.com>
2024-03-06 23:42:42 +0600

    ema-jhon-simple done

--- ema-john-simple/src/components/Cart/Cart.jsx
+++ ema-john-simple/src/components/Cart/Cart.jsx
@@ -2,15 +2,19 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import './Cart.css' ;
+import Product from '../Product/Product';
 
 const Cart = (props) => {
 
     const {cart} = props;
     let totalPrice = 0;
     let totalShoping = 0;
+    let quantity = 0;
     for (const item of cart) {
-        totalPrice += item.price;
+        item.quantity = item.quantity || 1;
+        totalPrice += item.price * item.quantity;
         totalShoping += item.shipping;
+        quantity = quantity + item.quantity;
     }
     const vatTax = totalPrice*5/100;
     const grandTotal = totalPrice + totalShoping + vatTax;
