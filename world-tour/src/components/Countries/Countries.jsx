0dad1cf03727135bc2e483a43c856a467136f217
hrid1 <hridoycse1@gmail.com>
2024-03-10 23:56:35 +0600

    world tour

---
+++ world-tour/src/components/Countries/Countries.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+
+const Countries = () => {
+
+    const [countries, setCountries] = useState([]);
+    useEffect( () => {
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    },[])
+
+ 
+
+
+    return (
+        <div>
+            
+            
+        </div>
+    );
+};
+
+export default Countries;
